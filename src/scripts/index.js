b34172a7a2dbe1badcd9d212256f7aacc41db4d2
Dede Rahmat <dederamat102@gmail.com>
2024-12-15 19:38:01 +0700

    fixing responsive on mobile, adding drawer and adding feature to send email contact me

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,47 @@
 import '../sass/main.scss';
 
-console.log('Loaded');
+const DrawerInitiator = {
+  init({ button, drawer, header, content }) {
+    button.addEventListener('click', (event) => {
+      this._toggleDrawer(event, drawer, button);
+    });
+    
+    header.addEventListener('click', (event) => {
+      this._closeDrawer(event, drawer, button);
+    });
+    
+    content.addEventListener('click', (event) => {
+      this._closeDrawer(event, drawer, button);
+    });
+  },
+
+  _toggleDrawer(event, drawer, button) {
+    event.stopPropagation();
+    drawer.classList.toggle('open');
+
+    // Perubahan teks tombol
+    if (drawer.classList.contains('open')) {
+      button.innerText = '\u2715';
+    } else {
+      button.innerText = '☰';
+    }
+  },
+  
+  _closeDrawer(event, drawer, button) {
+    event.stopPropagation();
+    drawer.classList.remove('open');
+    button.innerText = '☰';
+  },
+}
+
+const buttonMenu = document.querySelector('#menu');
+const drawer = document.querySelector('#drawer');
+const header = document.querySelector('header');
+const content = document.querySelector('main');
+
+DrawerInitiator.init({
+  button: buttonMenu,
+  drawer: drawer,
+  header: header,
+  content: content,
+});
